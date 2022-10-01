ac10bc5359531b11368a931897ef2502e1dd5094
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-10-01 15:19:28 +0500

    Solve 1. Lessons Getter

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,31 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
+    public static void main(String[] args) {
+        LessonsGetter lg = new LessonsGetter();
+        lg.getLessons(
+                Map.of(
+                        "Monday", List.of("English", "Mathematics", "Chemistry"),
+                        "Tuesday", List.of("Biology", "Informatics"),
+                        "Wednesday", List.of("Geography", "Astronomy", "English"),
+                        "Thursday", List.of("Mathematics", "Literature"),
+                        "Friday", List.of("Physics", "Technology", "English")
+                )
+        );
+    }
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> result = new HashSet<>();
+        for (Map.Entry<String, List<String>> set : timetable.entrySet()) {
+            for (String val : set.getValue()) {
+                result.add(val);
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,13 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        return result;
     }
 }
